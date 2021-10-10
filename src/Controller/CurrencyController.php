9dcd10fbf333472440fd4c35b557b9bd632ae9cb
Matthias Straka <mstraka84@gmail.com>
2021-10-10 11:03:47 +0200

    Show and delete currencies

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class CurrencyController extends AbstractController
+{
+    /**
+     * @Route("/currency", name="currency")
+     */
+    public function index(): Response
+    {
+        $currencies = $this->getDoctrine()
+            ->getRepository(\App\Entity\Currency::class)
+            ->findAll();
+
+        return $this->render('currency/index.html.twig', [
+            'controller_name' => 'CurrencyController',
+            'currencies' => $currencies
+        ]);
+    }
+
+    /**
+     * @Route("/currency/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+        $currency = $this->getDoctrine()->getRepository(\App\Entity\Currency::class)->find($id);
+  
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($currency);
+        $entityManager->flush();
+  
+        $response = new Response();
+        $response->send();
+    }
+}
