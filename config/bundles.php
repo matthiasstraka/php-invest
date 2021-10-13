462c301870e0173bb67774df66bacd9fa4fef2ba
Matthias Straka <mstraka84@gmail.com>
2021-10-13 10:41:57 +0200

    Install profiler for dev

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
