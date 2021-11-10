c2d61d72a857488c33714a5a4bad5c4b9550ef4a
Matthias Straka <mstraka84@gmail.com>
2021-11-10 10:27:19 +0100

    Change transaction table name to prevent sqlite problems

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -8,6 +8,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TransactionRepository::class)
+ * @ORM\Table(name="account_transaction")
  */
 class Transaction
 {
