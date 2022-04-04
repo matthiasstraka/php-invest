06107de3e3176ad2da76ce67ad49e0746b6d82f2
Matthias Straka <mstraka84@gmail.com>
2022-04-04 14:07:21 +0200

    Allow to enter non-trade related interest input

--- src/Form/TransactionType.php
+++ src/Form/TransactionType.php
@@ -31,9 +31,10 @@ class TransactionType extends AbstractType
 
         $builder
             ->add('time', DateTimeType::class, ['label' => 'Time', 'date_widget' => 'single_text', 'time_widget' => 'single_text', 'with_seconds' => true])
-            ->add('transaction_id', NumberType::class, ['html5' => true, 'input' => 'string', 'required' => false, 'help' => 'Transaction ID used by your broker'])
+            ->add('transaction_id', NumberType::class, ['html5' => true, 'required' => false, 'help' => 'Transaction ID used by your broker'])
             ->add('cash', MoneyType::class, ['required' => false, 'html5' => false, 'currency' => $currency, 'scale' => 4, 'help' => 'Cash input/output'])
             ->add('consolidation', MoneyType::class, ['required' => false, 'html5' => false, 'currency' => $currency, 'scale' => 4, 'help' => 'Correction if there is a balance mismatch'])
+            ->add('interest', MoneyType::class, ['required' => false, 'html5' => false, 'currency' => $currency, 'scale' => 4, 'help' => 'Interest costs (usually negative)'])
             ->add('consolidated', CheckboxType::class, ['required' => false, 'help' => 'Check if this transaction matches with your broker'])
             ->add('notes', TextareaType::class, ['required' => false])
             ->add('save', SubmitType::class, ['label' => 'Submit', 'attr' => ['class' => 'btn btn-primary']])
