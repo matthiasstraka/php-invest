66acfbb6ca6530abf31272eb8c32562c1c250261
Matthias Straka <mstraka84@gmail.com>
2022-06-07 21:14:38 +0200

    service: convert multiple prices with unit test

--- src/Service/InstrumentPriceService.php
+++ src/Service/InstrumentPriceService.php
@@ -20,6 +20,32 @@ class InstrumentPriceService
         $this->fx_xchg = $fx_xchg;
     }
 
+    private static function createScaledPrice(Instrument $instrument, AssetPrice $asset_price, string $scale): InstrumentPrice
+    {
+        $ip = new InstrumentPrice();
+        $ip->setInstrument($instrument);
+        $ip->setDate($asset_price->getDate());
+        $ip->setOHLC(
+            bcmul($scale, $asset_price->getOpen(), 4),
+            bcmul($scale, $asset_price->getHigh(), 4),
+            bcmul($scale, $asset_price->getLow(), 4),
+            bcmul($scale, $asset_price->getClose(), 4));
+        return $ip;
+    }
+
+    private static function createKnockoutPrice(Instrument $instrument, AssetPrice $asset_price, string $scale, string $strike): InstrumentPrice
+    {
+        $ip = new InstrumentPrice();
+        $ip->setInstrument($instrument);
+        $ip->setDate($asset_price->getDate());
+        $ip->setOHLC(
+            bcmul($scale, bcsub($asset_price->getOpen(), $strike, 6), 4),
+            bcmul($scale, bcsub($asset_price->getHigh(), $strike, 6), 4),
+            bcmul($scale, bcsub($asset_price->getLow(), $strike, 6), 4),
+            bcmul($scale, bcsub($asset_price->getClose(), $strike, 6), 4));
+        return $ip;
+    }
+
     public function fromAssetPrice(Instrument $instrument, AssetPrice $asset_price, ?InstrumentTerms $terms = null): ?InstrumentPrice
     {
         if ($instrument->hasTerms() && $terms == null)
@@ -41,12 +67,7 @@ class InstrumentPriceService
         switch ($instrument->getEusipa()) {
             case Instrument::EUSIPA_UNDERLYING:
             case Instrument::EUSIPA_CFD:
-                $ip->setOHLC(
-                    bcmul($fx_factor, $asset_price->getOpen(), 4),
-                    bcmul($fx_factor, $asset_price->getHigh(), 4),
-                    bcmul($fx_factor, $asset_price->getLow(), 4),
-                    bcmul($fx_factor, $asset_price->getClose(), 4));                
-                break;
+                return self::createScaledPrice($instrument, $asset_price, $fx_factor);
 
             case Instrument::EUSIPA_MINIFUTURE:
             case Instrument::EUSIPA_KNOCKOUT:
@@ -61,18 +82,53 @@ class InstrumentPriceService
                     $strike = "0";
                 }
 
-                $ip->setOHLC(
-                    bcmul($factor, bcsub($asset_price->getOpen(), $strike, 6), 4),
-                    bcmul($factor, bcsub($asset_price->getHigh(), $strike, 6), 4),
-                    bcmul($factor, bcsub($asset_price->getLow(), $strike, 6), 4),
-                    bcmul($factor, bcsub($asset_price->getClose(), $strike, 6), 4)
-                );
+                return self::createKnockoutPrice($instrument, $asset_price, $factor, $strike);
+        }
+        return null;
+    }
+
+    public function fromAssetPrices(Instrument $instrument, array $asset_price, ?InstrumentTerms $terms = null): array
+    {
+        if ($instrument->hasTerms() && $terms == null)
+        {
+            return [];
+        }
+
+        // TODO: search best conversion rate near $asset_price->getDate()
+        $fx_factor = $this->fx_xchg->latestConversion($instrument->getUnderlying()->getCurrency(), $instrument->getCurrency());
+        if ($fx_factor == null)
+        {
+            return [];
+        }
+
+        $result = [];
+
+        switch ($instrument->getEusipa()) {
+            case Instrument::EUSIPA_UNDERLYING:
+            case Instrument::EUSIPA_CFD:
+                foreach ($asset_price as $ap) {
+                    $result[] = self::createScaledPrice($instrument, $ap, $fx_factor);
+                }
                 break;
 
-            default:
-                return null; // not supported
+            case Instrument::EUSIPA_MINIFUTURE:
+            case Instrument::EUSIPA_KNOCKOUT:
+            case Instrument::EUSIPA_CONSTANT_LEVERAGE:
+                if ($terms == null)
+                    return [];
+                $factor = doubleval($terms->getRatio()) * $instrument->getDirection() * doubleval($fx_factor);
+
+                $strike = $terms->getStrike();
+                if ($strike == null)
+                {
+                    $strike = "0";
+                }
+                foreach ($asset_price as $ap) {
+                    $result[] = self::createKnockoutPrice($instrument, $ap, $factor, $strike);
+                }
+                break;
         }
-        return $ip;
+        return $result;
     }
 
     public function latestPrice(Instrument $instrument, ?InstrumentTerms $terms = null): ?InstrumentPrice
---
+++ tests/InstrumentPriceServiceTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Asset;
+use App\Entity\AssetPrice;
+use App\Entity\Instrument;
+use App\Service\InstrumentPriceService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class InstrumentPriceServiceTest extends KernelTestCase
+{
+    protected $asset;
+    protected $instrument;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $this->asset = new Asset();
+        $this->asset->setCurrency("USD");
+
+        $this->instrument = new Instrument();
+        $this->instrument->setUnderlying($this->asset);
+        $this->instrument->setEusipa(Instrument::EUSIPA_UNDERLYING);
+        $this->instrument->setCurrency("EUR");
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testSingle(): void
+    {
+        $ips = static::getContainer()->get(InstrumentPriceService::class);
+
+        $ap = new AssetPrice();
+        $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2021-10-12 00:00:00');
+        $ap->setDate($d);
+        $ap->setAsset($this->asset);
+        $ap->setOHLC(100, 110, 90, 105);
+
+        $eurusd = 0.9315;
+
+        $ip = $ips->fromAssetPrice($this->instrument, $ap);
+        $this->assertIsObject($ip);
+        $this->assertEquals($ap->getDate(), $ip->getDate());
+        $this->assertEquals($this->instrument, $ip->getInstrument());
+        $this->assertEquals($ap->getOpen() * $eurusd, $ip->getOpen());
+        $this->assertEquals($ap->getHigh() * $eurusd, $ip->getHigh());
+        $this->assertEquals($ap->getLow() * $eurusd, $ip->getLow());
+        $this->assertEquals($ap->getClose() * $eurusd, $ip->getClose());
+    }
+
+    public function testArray(): void
+    {
+        $ips = static::getContainer()->get(InstrumentPriceService::class);
+
+        $ap1 = new AssetPrice();
+        $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2021-10-12 00:00:00');
+        $ap1->setDate($d);
+        $ap1->setAsset($this->asset);
+        $ap1->setOHLC(100, 110, 90, 105);
+
+        $ap2 = new AssetPrice();
+        $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2021-10-13 00:00:00');
+        $ap2->setDate($d);
+        $ap2->setAsset($this->asset);
+        $ap2->setOHLC(1000, 1100, 900, 1050);
+
+        $prices = [$ap1, $ap2];
+
+        $eurusd = 0.9315;
+
+        $ip = $ips->fromAssetPrices($this->instrument, $prices);
+        $this->assertIsArray($ip);
+        for ($i = 0; $i < 2; $i++) {
+            $this->assertEquals($prices[$i]->getDate(), $ip[$i]->getDate());
+            $this->assertEquals($this->instrument, $ip[$i]->getInstrument());
+            $this->assertEquals($prices[$i]->getOpen() * $eurusd, $ip[$i]->getOpen());
+            $this->assertEquals($prices[$i]->getHigh() * $eurusd, $ip[$i]->getHigh());
+            $this->assertEquals($prices[$i]->getLow() * $eurusd, $ip[$i]->getLow());
+            $this->assertEquals($prices[$i]->getClose() * $eurusd, $ip[$i]->getClose());
+        }
+    }
+}
