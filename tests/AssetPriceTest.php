505932399cad14d830182ff0d1e122432c7fb20a
Matthias Straka <mstraka84@gmail.com>
2022-06-07 20:36:12 +0200

    unittest: correct order of arguments

--- tests/AssetPriceTest.php
+++ tests/AssetPriceTest.php
@@ -10,8 +10,8 @@ class AssetPriceTest extends TestCase
     public function testDateConversion(): void
     {
         $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2022-04-06 00:00:00');
-        $this->assertEquals(AssetPrice::getDateValue($d), 19088);
-        $this->assertEquals(AssetPrice::valueToDate(19088), $d);
+        $this->assertEquals(19088, AssetPrice::getDateValue($d));
+        $this->assertEquals($d, AssetPrice::valueToDate(19088));
     }
 
     public function testDate(): void
@@ -19,27 +19,27 @@ class AssetPriceTest extends TestCase
         $ap = new AssetPrice();
         $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2021-10-12 00:00:00');
         $ap->setDate($d);
-        $this->assertEquals($ap->getDate(), $d);
-        $this->assertEquals($ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '1970-01-01')), 0);
-        $this->assertEquals($ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '2022-02-16')), 19039);
+        $this->assertEquals($d, $ap->getDate());
+        $this->assertEquals(0, $ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '1970-01-01')));
+        $this->assertEquals(19039, $ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '2022-02-16')));
     }
 
     public function testVolume(): void
     {
         $ap = new AssetPrice();
-        $this->assertEquals($ap->getVolume(), 0);
+        $this->assertEquals(0, $ap->getVolume());
         $ap->setVolume(1234);
-        $this->assertEquals($ap->getVolume(), 1234);
+        $this->assertEquals(1234, $ap->getVolume());
     }
 
     public function testOHLC(): void
     {
         $ap = new AssetPrice();
         $ap->setOHLC(1.2,5.7,-2.55,10000);
-        $this->assertEquals($ap->getOpen(), 1.2);
-        $this->assertEquals($ap->getHigh(), 5.7);
-        $this->assertEquals($ap->getLow(), -2.55);
-        $this->assertEquals($ap->getClose(), 10000);
-        $this->assertEquals($ap->getClose(), '10000');
+        $this->assertEquals(1.2, $ap->getOpen());
+        $this->assertEquals(5.7, $ap->getHigh());
+        $this->assertEquals(-2.55, $ap->getLow());
+        $this->assertEquals(10000, $ap->getClose());
+        $this->assertEquals('10000', $ap->getClose());
     }
 }
--- tests/InstrumentPriceTest.php
+++ tests/InstrumentPriceTest.php
@@ -10,8 +10,8 @@ class InstrumentPriceTest extends TestCase
     public function testDateConversion(): void
     {
         $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2022-04-06 00:00:00');
-        $this->assertEquals(InstrumentPrice::getDateValue($d), 19088);
-        $this->assertEquals(InstrumentPrice::valueToDate(19088), $d);
+        $this->assertEquals(19088, InstrumentPrice::getDateValue($d));
+        $this->assertEquals($d, InstrumentPrice::valueToDate(19088));
     }
 
     public function testDate(): void
@@ -19,19 +19,19 @@ class InstrumentPriceTest extends TestCase
         $ap = new InstrumentPrice();
         $d = \DateTime::createFromFormat("Y-m-d H:i:s", '2021-10-12 00:00:00');
         $ap->setDate($d);
-        $this->assertEquals($ap->getDate(), $d);
-        $this->assertEquals($ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '1970-01-01')), 0);
-        $this->assertEquals($ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '2022-02-16')), 19039);
+        $this->assertEquals($d, $ap->getDate());
+        $this->assertEquals(0, $ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '1970-01-01')));
+        $this->assertEquals(19039, $ap->getDateValue(\DateTime::createFromFormat("Y-m-d", '2022-02-16')));
     }
 
     public function testOHLC(): void
     {
         $ap = new InstrumentPrice();
         $ap->setOHLC(1.2,5.7,-2.55,10000);
-        $this->assertEquals($ap->getOpen(), 1.2);
-        $this->assertEquals($ap->getHigh(), 5.7);
-        $this->assertEquals($ap->getLow(), -2.55);
-        $this->assertEquals($ap->getClose(), 10000);
-        $this->assertEquals($ap->getClose(), '10000');
+        $this->assertEquals(1.2, $ap->getOpen());
+        $this->assertEquals(5.7, $ap->getHigh());
+        $this->assertEquals(-2.55, $ap->getLow());
+        $this->assertEquals(10000, $ap->getClose());
+        $this->assertEquals('10000', $ap->getClose());
     }
 }
