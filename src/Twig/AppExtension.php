c13bd77fa5583ff8d8bb9f7176e77d6cefbf0022
Matthias Straka <mstraka84@gmail.com>
2022-09-06 15:31:25 +0200

    Link asset with currency if available

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,16 +2,26 @@
 
 namespace App\Twig;
 
+use App\Entity\Asset;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AppExtension extends AbstractExtension
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function getFilters(): array
     {
         return [
             new TwigFilter('flag_icon', [$this, 'flagIcon'], ['is_safe' => ['html']]),
             new TwigFilter('symbol_badge', [$this, 'symbolBadge'], ['is_safe' => ['html']]),
+            new TwigFilter('asset_from_isin', [$this, 'assetFromIsin'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -31,5 +41,21 @@ class AppExtension extends AbstractExtension
 
         $symbol = strtoupper($symbol);
         return "<span class=\"badge bg-secondary\">$symbol</span>";
+    }
+
+    public function assetFromIsin(?string $isin): int
+    {
+        if ($isin == null)
+            return 0;
+
+        $isin = strtoupper($isin);
+
+        $repo = $this->entityManager->getRepository(Asset::class);
+        $asset = $repo->findOneBy(['ISIN' => $isin]);
+        if ($asset)
+        {
+            return $asset->getId();
+        }
+        return 0;
     }
 }
