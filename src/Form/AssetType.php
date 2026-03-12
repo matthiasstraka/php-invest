b81373807aad4d911cbacedee0fb5571dccfa1d5
Matthias Straka <mstraka84@gmail.com>
2026-03-12 21:49:09 +0100

    Replace custom country list by symfony CountryType #58

--- src/Form/AssetType.php
+++ src/Form/AssetType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
